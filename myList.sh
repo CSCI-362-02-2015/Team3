530bd20c103003899bb2ea78736f6042089dfdd1
Cassios <cassios.kmm@gmail.com>
2015-08-30 22:07:27 -0400

    merger files

--- myList.sh
+++ myList.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-rm test.html
-sh ./html-template.sh >> test.html
+
+getHTML() {
+	echo "<!doctype html>"
+	echo "<html>"
+	echo "<head>"
+	echo "<title>Top-level directory</title>"
+	echo "</head>"
+	echo "<body>"
+	echo "<h5>List of all items of the current folder</h5>"
+	echo "<ul>"
+	for file in *
+	do
+		echo "<li>$file"
+	done
+	echo "</ul>"
+	echo "</body>"
+	echo "</html>"
+}
+
+getHTML > test.html
 firefox test.html
