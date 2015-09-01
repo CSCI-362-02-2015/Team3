c3f26d5c9f412e0b0a8f0f0894a9e0da2e218268
root <root@localhost.localdomain>
2015-09-01 14:09:15 -0400

    I'm adding some graphical improvements.

--- myList.sh
+++ myList.sh
@@ -4,14 +4,17 @@ getHTML() {
 	echo "<!doctype html>"
 	echo "<html>"
 	echo "<head>"
+	echo "<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">"
 	echo "<title>Top-level directory</title>"
 	echo "</head>"
 	echo "<body>"
-	echo "<h5>List of all items of the current folder</h5>"
-	echo "<ul>"
+	echo "<div style='margin-left:15px' class='page-header'>
+ 		<h1>List of all items of the currend folder <small>International Team 3</small></h1>
+		</div>"
+	echo "<ul style='margin-left:15px; float: left; width: auto' class='list-group'>"
 	for file in *
 	do
-		echo "<li>$file"
+		echo "<li class='list-group-item'>$file"
 	done
 	echo "</ul>"
 	echo "</body>"
