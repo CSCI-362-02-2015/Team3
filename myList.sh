3d63279c41664a9167b473ee7a79070c94be9791
Cassios <cassios.kmm@gmail.com>
2015-09-02 00:24:02 -0400

    implement dinymic subdirectory deep

--- myList.sh
+++ myList.sh
@@ -1,38 +1,53 @@
 #Team3
 #!/bin/bash
 
+#Usage: myList.sh <MAX_DEEP>
+#eg: myList.sh 4
+
+
+if [ -z "$1" ]; then #if no parameter, set 3 as default
+	MAX_DEEP=3
+else
+	MAX_DEEP=$[$1]
+fi
+DEEP=0
+listSubDirectories() {
+	DEEP=$[DEEP + 1]
+	echo "<ul>"
+	for file in *; do
+		echo "<li>$file"
+		if [[ -d $file ]]; then
+			if [[ $DEEP -lt $MAX_DEEP ]]; then
+				cd "$file"
+				listSubDirectories
+				cd ..
+			fi
+		fi
+		echo "</li>"
+	done
+	echo "</ul>"
+	DEEP=$[DEEP - 1]
+}
+
 getHTML() {
+	ACTUAL_DIR=${PWD##*/} 
 	echo "<!doctype html>"
 	echo "<html>"
 	echo "<head>"
 	echo "<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">"
-	echo "<title>Top-level directory</title>"
+	echo "<title>MyList Script</title>"
 	echo "</head>"
 	echo "<body>"
 	echo "<div style='margin-left:15px' class='page-header'>
- 		<h1>List of all items of the currend folder <small>International Team 3</small></h1>
+		<h1>International Team 3</h1>
+		<h2>Files at directory <font color='red'>$ACTUAL_DIR</font> with max deep <font color='red'>$MAX_DEEP</font></h2>
 		</div>"
-	echo "<ul style='margin-left:15px; float: left; width: auto' class='list-group'>"
-	for file in *
-	do
-		if [ -d "$file" ] 
-			then 
-				cd $file
-				echo "<div class='list-group-item'><b>$file ($(ls -1 | wc -l) files)</b>"
-				if [ "$(ls -A *)" ]
-					then for insideFile in *
-						do echo "<li style='margin: 0 0 0 50px'>$insideFile"
-					done
-				fi
-				echo "</div>"
-				cd ..
-			else echo "<div class='list-group-item'>$file <span style='margin-left:15px;' class="badge">$(stat -c%s "$file") bytes</span></div>"
-		fi
-	done
-	echo "</ul>"
+
+	listSubDirectories
+
 	echo "</body>"
 	echo "</html>"
 }
 
 getHTML > test.html
-firefox -url test.html 2>/dev/null
+firefox test.html
