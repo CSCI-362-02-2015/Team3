217956e64065dce811b87095c25d97ae88bebaac
cassios <cassios.kmm@gmail.com>
2015-10-20 15:34:18 -0400

    compares expected result and output and generate html file

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -5,38 +5,37 @@ NC='\033[0m' # No Color
 function executeTestCase() {
 # it goes through every line of a test case, get the executable, the parameters,
 # runs it, and compares the result with the expected outcome
+    MODULE=''
+    FUNCTION=''
+    IT=''
+    DRIVER=''
+    ARGUMENTS=''
+    EXPECTED=''
     while read line; do
         var=$(echo "$line" | awk '{print $1;}')
         arg=$(echo "$line" | awk -F\" '{print $2}')
         case $var in
-            module ) echo "MODULE" $arg ;;
-            function ) echo "FUNCTION" $arg ;;
-            it ) echo "IT" $arg ;;
-            driver )
-                echo "DRIVER" $arg ;;
-                # for word in $arg; do
-                #     if [ ${word: -2} == ".c" ]
-                #         then files+=($word)
-                #     fi
-                # done
-                # for file in $files; do
-                #     # filename without extension
-                #     filename=(${file//./ })
-                #     gcc -o "../temp/$filename" "../testCasesExecutables/$file"
-                # done
-                # echo -e "===> ${GREEN}All files compiled${NC}" ;;
-            arguments )
-                echo "ARGUMENTS" $arg
-                for word in $arg; do
-                    if [ ${word: -2} == ".c" ]
-                        then arguments+=($word)
-                    fi
-                done ;;
-            expected )
-                echo "EXPECTED" $arg ;;
+            module ) MODULE=$arg ;;
+            function ) FUNCTION=$arg ;;
+            it ) IT=$arg ;;
+            driver ) DRIVER=$arg ;;
+            arguments ) ARGUMENTS=$arg ;;
+            expected ) EXPECTED=$arg ;;
         esac
     done <"$1" #file named passed as argument
-    echo -e "###################################\n"
+
+    result=$(./$DRIVER $ARGUMENTS)
+    echo "MODULE $MODULE" >> "$ROOT/temp/output.txt"
+    echo "FUNCTION $FUNCTION" >> "$ROOT/temp/output.txt"
+    echo "IT $IT" >> "$ROOT/temp/output.txt"
+    if [ $result == $EXPECTED ]; then
+        echo "passed" >> "$ROOT/temp/output.txt"
+    else
+        echo "failed" >> "$ROOT/temp/output.txt"
+        echo -e "\tEXPECTED $EXPECTED" >> "$ROOT/temp/output.txt"
+        echo -e "\tOUTPUT $result" >> "$ROOT/temp/output.txt"
+    fi
+    echo -e "###################################\n" >> "$ROOT/temp/output.txt"
 }
 
 
@@ -52,6 +51,10 @@ echo -e "Finished.\n"
 
 echo -e "Number of test cases: ${testCasesFiles[@]} \n"
 
+rm "$ROOT/temp/output.txt"
+
 for testCaseFile in "${testCasesFiles[@]}"; do #iterate over the test cases
     executeTestCase "$testCaseFile"
 done
+
+firefox "$ROOT/temp/output.txt" &> /dev/null
